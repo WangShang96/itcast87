89d361de9cc051606fdae51ccbf3d5db7a0cfa75
WangShang96 <1102887969@qq.com>
2019-04-21 10:55:50 +0800

    test3用ssh的方式提交并推送了

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("test2修改了!");
+		System.out.println("test3修改了!");
 		
     }
 }
